32ac6db6d86bbe86569ff65fb295aa4cdf27a387
Andrew Grace <apenngrace@users.noreply.github.com>
2018-12-03 09:09:33 -0500

    fixed version again.

--- Casks/vulkan-sdk.rb
+++ Casks/vulkan-sdk.rb
@@ -1,10 +1,8 @@
 cask 'vulkan-sdk' do
-  version '1.1.92.1_1'
+  version '1.1.92.1'
   sha256 '1dc5c758ba83cc0b1e3baa533a5b2052afa378df87a84ee3e56ab6d97df12865'
-  
-  tmp_version = '1.1.92.1'
 
-  url "https://sdk.lunarg.com/sdk/download/#{tmp_version}/mac/vulkansdk-macos-#{tmp_version}.tar.gz"
+  url "https://sdk.lunarg.com/sdk/download/#{version}/mac/vulkansdk-macos-#{version}.tar.gz"
   name 'LunarG Vulkan SDK'
   homepage 'https://vulkan.lunarg.com/sdk/home'
 
@@ -24,7 +22,7 @@ cask 'vulkan-sdk' do
   DEST_ICD          = "/usr/local/share/vulkan/icd.d"
   DEST_LAYER        = "/usr/local/share/vulkan/explicit_layer.d"
   
-  mylist = tmp_version.split(".")
+  mylist = version.split(".")
   lib_version = mylist[0] + "." + mylist[1] + "." + mylist[2]
 
   #Vulkan Executable Binaries to Install
